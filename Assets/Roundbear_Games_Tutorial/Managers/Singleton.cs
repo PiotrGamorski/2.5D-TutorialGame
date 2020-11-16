dd56dc07ec390174354c8e11f7e5e29ac7c8e1c3
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2020-11-16 16:04:17 +0100

    Project Initialized

---
+++ Assets/Roundbear_Games_Tutorial/Managers/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance = null;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject();
+                    go.name = typeof(T).ToString();
+                    _instance = go.AddComponent<T>();
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Assets/Roundbear_Games_Tutorial/TestingCube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    public class TestingCube : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            if (VirtualInputManager.Instance.MoveLeft && VirtualInputManager.Instance.MoveRight)
+            {
+                return;
+            }
+
+            if (VirtualInputManager.Instance.MoveRight)
+            {
+                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                this.gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+
+            if (VirtualInputManager.Instance.MoveLeft)
+            {
+                this.gameObject.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                this.gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+        }
+    }
+}
